4517f7552f1d10942c810dedb5d6cced12c97b5a
Wally <wolaondo@gmail.com>
2017-12-09 11:27:28 -0300

    adding test user authenticated

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -170,4 +170,21 @@ describe('PATCH /todos/:id', () => {
     })
     .end(done);
   });
-})
+});
+
+//testing Get users/me
+describe('GET /users/me',() => {
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)//configura el header q se envia
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+  
+
+});
